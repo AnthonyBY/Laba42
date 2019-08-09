db8bd78f6033c240f2506eefef3f96b86411c60a
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-09 21:57:22 +0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {omniauth_callbacks: 'users/omniauth_callbacks'}
   root to: 'home#index'
   resources :authentications, only: [:destroy]
+  match '/auth/facebook/callback' => 'services#create'
 end
