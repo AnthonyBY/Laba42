c8ee46da5059085bce7da00aa70d631266afe1f4
Almix047 <Amix047@gmail.com>
2019-08-10 17:51:14 +0000

    add i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,15 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    {locale: I18n.locale}
+  end
 
   protected
 
