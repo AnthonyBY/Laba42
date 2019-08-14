7228c59520df4252f180f13019cd0a304f5508a2
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-14 12:08:44 +0300

    Added array default and null false to user migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
+# rubocop:disable Layout/SpaceInsideHashLiteralBraces
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
 end
+# rubocop:enable Layout/SpaceInsideHashLiteralBraces
