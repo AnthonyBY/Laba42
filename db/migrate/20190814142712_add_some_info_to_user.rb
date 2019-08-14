523e414cdbf49d161a59874c581d71f14a837b0b
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-14 18:00:33 +0300

    Remove some info from user

--- db/migrate/20190814142712_add_some_info_to_user.rb
+++
@@ -1,9 +0,0 @@
-class AddSomeInfoToUser < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :first_name, :string, default: "", null: false
-    add_column :users, :surname_name, :string, default: "", null: false
-    add_column :users, :company_name, :string, default: "", null: false
-    add_column :users, :phone_number, :string, default: "", null: false
-    add_column :users, :city, :string, default: "", null: false
-  end
-end
