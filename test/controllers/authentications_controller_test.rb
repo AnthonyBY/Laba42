99ae26e330d0c91fc987b58c9f12cf670b47e8ff
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-14 18:26:43 +0300

    Vestigial project files deleted

--- test/controllers/authentications_controller_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class AuthenticationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
