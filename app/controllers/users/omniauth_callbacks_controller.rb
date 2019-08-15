ac04aac6aca34575b5f3d113c05497cee457f4e3
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-15 09:27:36 +0300

    hotfix google oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -6,7 +6,7 @@ module Users
     def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
       if @user.persisted?
-        sign_in @user, event: authentication # this will throw if @user is not activated
+        sign_in @user, event: :authentication # this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
         session['devise.google_data'] = request.env['omniauth.auth']
