bc533757d6b918a3a4e21db704a7c8102a7a81ac
middaysan <midda7@mail.ru>
2019-08-15 18:23:05 +0300

    robucop issues fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_locale
-  before_action :authenticate_user!, only: %i[select_role edit]
+  before_action :authenticate_user!, only: %i[select_role edit_user_info]
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
