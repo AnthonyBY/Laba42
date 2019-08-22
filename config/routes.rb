298386ba67739390e6c22d54120c3dd88ce48414
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-22 12:30:21 +0300

    Generate users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
 
