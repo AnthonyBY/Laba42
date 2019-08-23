2b15fba75eb336c87ef8d38cac591b70167ef0d3
Almix047 <Amix047@gmail.com>
2019-08-23 17:21:47 +0000

    begin write 'destroy' action in 'applies_controller'

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -2,32 +2,18 @@
 
 class AppliesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user_id
 
   def create
-    @apply = Project.find(set_project_id).applies.new(user_id: set_user_id)
+    @apply = Apply.new(project_id: params[:project], user_id: current_user.id)
     if @apply.save
-      redirect_to "/projects/#{set_project_id}", notice: 'Apply was successfully created.'
+      redirect_to "/projects/#{params[:project]}", notice: 'Apply was successfully created.'
     else
-      redirect_to "/projects/#{set_project_id}", notice: 'We have a problem.'
+      redirect_to "/projects/#{params[:project]}", notice: 'We have a problem.'
     end
   end
 
   def destroy
+    binding.pry
     @apply.destroy
-    respond_to do |format|
-      format.html { redirect_to applies_url, notice: 'Apply was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
-  private
-
-  def set_project_id
-    params[:project]
-  end
-
-  def set_user_id
-    current_user.id
   end
 end
