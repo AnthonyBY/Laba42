acd0518c760db8669a48b98c4d0c77adddfedc97
Almix047 <Amix047@gmail.com>
2019-08-23 14:06:37 +0000

    write 'create' action in 'applies_controller'

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -1,58 +1,18 @@
+# frozen_string_literal: true
+
 class AppliesController < ApplicationController
-  before_action :set_apply, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+  before_action :set_user_id
 
-  # GET /applies
-  # GET /applies.json
-  def index
-    @applies = Apply.all
-  end
-
-  # GET /applies/1
-  # GET /applies/1.json
-  def show
-  end
-
-  # GET /applies/new
-  def new
-    @apply = Apply.new
-  end
-
-  # GET /applies/1/edit
-  def edit
-  end
-
-  # POST /applies
-  # POST /applies.json
   def create
-    @apply = Apply.new(apply_params)
-
-    respond_to do |format|
-      if @apply.save
-        format.html { redirect_to @apply, notice: 'Apply was successfully created.' }
-        format.json { render :show, status: :created, location: @apply }
-      else
-        format.html { render :new }
-        format.json { render json: @apply.errors, status: :unprocessable_entity }
-      end
+    @apply = Project.find(set_project_id).applies.new(user_id: set_user_id)
+    if @apply.save
+      redirect_to "/projects/#{set_project_id}", notice: 'Apply was successfully created.'
+    else
+      redirect_to "/projects/#{set_project_id}", notice: 'We have a problem.'
     end
   end
 
-  # PATCH/PUT /applies/1
-  # PATCH/PUT /applies/1.json
-  def update
-    respond_to do |format|
-      if @apply.update(apply_params)
-        format.html { redirect_to @apply, notice: 'Apply was successfully updated.' }
-        format.json { render :show, status: :ok, location: @apply }
-      else
-        format.html { render :edit }
-        format.json { render json: @apply.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /applies/1
-  # DELETE /applies/1.json
   def destroy
     @apply.destroy
     respond_to do |format|
@@ -62,13 +22,12 @@ class AppliesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_apply
-      @apply = Apply.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def apply_params
-      params.fetch(:apply, {})
-    end
+  def set_project_id
+    params[:project]
+  end
+
+  def set_user_id
+    current_user.id
+  end
 end
