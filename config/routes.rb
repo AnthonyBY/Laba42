1b7f9e42e0f9ee0eccba75d0831d454ef933a364
Alex Samets <aliaksandr.samets@gmail.com>
2019-08-26 09:56:15 +0300

    Fix mentors issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/index'
+  resources :users, only: :index
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'home#index'
 
