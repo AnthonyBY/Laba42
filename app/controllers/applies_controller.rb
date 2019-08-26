ed2e86e2c0db47c2b59c43a011897e45770035ba
Almix047 <Amix047@gmail.com>
2019-08-26 00:22:25 +0000

    rename 'params[:project]' to 'params[:project_id]'

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -8,11 +8,11 @@ class AppliesController < ApplicationController
   def index; end
 
   def create
-    @apply = Apply.new(project_id: params[:project], user_id: current_user.id)
+    @apply = Apply.new(project_id: params[:project_id], user_id: current_user.id)
     if @apply.save
       redirect_to applies_path
     else
-      redirect_to "/projects/#{params[:project]}", notice: 'We have a problem.'
+      redirect_to "/projects/#{params[:project_id]}", notice: 'We have a problem.'
     end
   end
 
