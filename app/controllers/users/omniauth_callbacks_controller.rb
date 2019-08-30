31fcf224d25a8b60c889dcaebc80d7d20d3e6723
Almix047 <Amix047@gmail.com>
2019-08-30 08:57:42 +0000

    add flash notice

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,15 +9,14 @@ module Users
         sign_in @user, event: :authentication # this will throw if @user is not activated
         set_flash_message(:notice, :success, kind: 'Google') if is_navigational_format?
       else
-        session['devise.google_data'] = request.env['omniauth.auth']
+        flash[:error] = "cannot login with same email"
       end
       redirect_to '/'
     end
 
     def facebook
       if  current_user.present?
-        current_user.apply_omniauth(request.env['omniauth.auth'])
-        redirect_to edit_user_registration_path, notice: 'Facebook Account Linked!'
+        flash[:error] = "cannot login with same email"
       else
         @user = User.from_omniauth(request.env['omniauth.auth'])
         sign_in_and_redirect @user
