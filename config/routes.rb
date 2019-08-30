29f566c96d961587ec0ace998558f7ec65982142
Almix047 <Amix047@gmail.com>
2019-08-30 10:57:00 +0000

    Change default locale
      - Changed the default locale to :ru
      - Changed view of the locale in the address bar for better view

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,24 @@ Rails.application.routes.draw do
       post 'email_exists'
     end
   end
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'home#index'
 
-  resources :projects
-  resources :comments
-  resources :applies
-  resource :profile, controller: 'profile', only: %i[edit update] do
-    post 'update_role'
-    get 'edit_role'
-    get 'customer_setup_info'
-    get 'developer_setup_info'
-    get 'customer_edit'
-    get 'developer_edit'
-    get 'cabinet'
+  devise_for :users, only: :omniauth_callbacks, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    devise_for :users, skip: :omniauth_callbacks
+    root to: 'home#index'
+
+    resources :projects
+    resources :comments
+    resources :applies
+    resource :profile, controller: 'profile', only: %i[edit update] do
+      post 'update_role'
+      get 'edit_role'
+      get 'customer_setup_info'
+      get 'developer_setup_info'
+      get 'customer_edit'
+      get 'developer_edit'
+      get 'cabinet'
+    end
   end
 end
