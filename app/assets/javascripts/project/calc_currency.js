9f980d770a57fdec3792c97b42d093dbf88dee0f
middaysan <midda7@mail.ru>
2019-09-19 15:04:52 +0300

    add js multi skills tags, first stage

--- app/assets/javascripts/project/calc_currency.js
+++ app/assets/javascripts/project/calc_currency.js
@@ -1,13 +1,23 @@
 document.addEventListener("turbolinks:load", function(){
-    let currency;
-
-    (function() {
-        $.getJSON( "http://www.nbrb.by/api/exrates/rates/145", {}).done(function( data ) {
-            currency = data.Cur_OfficialRate;
-        });
-    })();
-
     $("#new_project").ready(function() {
+        let radio_but_test = document.getElementById('test_project_checkbox');
+        let radio_but_project = document.getElementById('radio_but_project');
+        let currency_block = document.getElementById('currency_block');
+        let currency;
+
+        radio_but_test.onclick = function () {
+            currency_block.style.display = 'none';
+        };
+        radio_but_project.onclick = function () {
+            currency_block.style.display = 'flex';
+        };
+
+        (function() {
+            $.getJSON( "http://www.nbrb.by/api/exrates/rates/145", {}).done(function( data ) {
+                currency = data.Cur_OfficialRate;
+            });
+        })();
+
         let input = document.getElementById('currency_input');
         let calc_out =  document.getElementById('currency_calc_view');
         input.oninput = function() {
