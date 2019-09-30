617e69cbd7da0b76be1f21a1a591dd8eb522c040
Almix047 <Amix047@gmail.com>
2019-09-30 23:38:02 +0300

    configured devise mailer for account verification and password recovery

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,15 +74,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDMAIL_USERNAME'],
     password: ENV['SENDMAIL_PASSWORD'],
-    domain: ENV['MAIL_HOST'],
     address: ENV['SMTP_SERVER'],
     port: ENV['SMTP_PORT'],
     authentication: :plain,
-    enable_starttls_auto: true
+    enable_starttls_auto: true,
+    ssl: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
