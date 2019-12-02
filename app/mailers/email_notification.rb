d1b2dc84f8ff573a79337dfe16667b729ca59a24
Almix047 <Amix047@gmail.com>
2019-12-02 11:03:15 +0300

    Refactoring to fix the problem identified by Reek

--- app/mailers/email_notification.rb
+++ app/mailers/email_notification.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
-# This class smells of :reek:InstanceVariableAssumption
 class EmailNotification < ApplicationMailer
   default from: 'noreply@laba42.com'
 
   def apply_email
-    @user = params[:user]
-    mail(to: @user.email, subject: 'Вы получили отклик на размещенный проект.')
+    mail(to: getting_user.email, subject: 'Вы получили отклик на размещенный проект.')
   end
 
   def appointment_email
-    @user = params[:user]
-    mail(to: @user.email, subject: 'Вас утвердили испонителем для проекта')
+    mail(to: getting_user.email, subject: 'Вас утвердили испонителем для проекта')
   end
 
   def comment_email
+    mail(to: getting_user.email, subject: 'Ваш проект прокомментировали.')
+  end
+
+  private
+
+  def getting_user
     @user = params[:user]
-    mail(to: @user.email, subject: 'Ваш проект прокомментировали.')
   end
 end
