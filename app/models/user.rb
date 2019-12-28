66a801cdaa07c3ea2b72b84a280a4c9fc9af9189
Almix047 <Amix047@gmail.com>
2019-12-28 10:47:33 +0300

    Added validation to time_zone users table

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_many :applied_to_projects, through: :applies, class_name: 'Project', foreign_key: 'project_id', source: :user
   has_many :comments, dependent: :destroy
   validates :name, presence: true
+  validates :time_zone, inclusion: { in: ActiveSupport::TimeZone.all.map(&:name) }
   USER_ROLES = { customer: 0, developer: 1 }.freeze
   enum role: USER_ROLES
   SKILLS = [
