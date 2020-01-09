293cfda7f737bf4470793fc5049e65ca48f579b2
Almix047 <Amix047@gmail.com>
2020-01-09 14:35:44 +0300

    Added initial (during apply) message (temporary stub)

--- app/controllers/applies_controller.rb
+++ app/controllers/applies_controller.rb
@@ -7,10 +7,12 @@ class AppliesController < ApplicationController
 
   def index; end
 
+  # rubocop:disable Metrics/AbcSize
   def create
     @apply = Apply.new(apply_params.merge(user_id: current_user.id))
     if @apply.save
       EmailNotification.with(user: Project.find(params[:project_id]).user).apply_email.deliver_later
+      Message.create(body_message: 'Пользователь предлагает свою кондидатуру.', user_id: current_user.id, recipient_id: @apply.project.user.id)
       redirect_to "/projects/#{params[:project_id]}"
     else
       redirect_to "/projects/#{params[:project_id]}", alert: 'Что-то пошло не так...'
@@ -22,7 +24,6 @@ class AppliesController < ApplicationController
     redirect_to applies_path
   end
 
-  # rubocop:disable Metrics/AbcSize
   def appointment
     if Project.find(params[:project_id]).update(employee: params[:appointed_user])
       Apply.find(params[:apply_id]).update(apply_status: 'accept')
