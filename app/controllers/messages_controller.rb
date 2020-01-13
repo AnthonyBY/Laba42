bf841c8eefa1e37ca8f26f173110ff5be8bac18d
Almix047 <Amix047@gmail.com>
2020-01-13 14:37:21 +0300

    Added views to private messsage

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,24 @@ class MessagesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @messages = Message.all
+    @autors = Message
+              .includes(:user)
+              .where(recipient_id: current_user.id).map(&:user).uniq
+  end
+
+  def show
+    @messages = Message
+                .includes(:user)
+                .where('user_id = ? AND recipient_id = ?', params[:id], current_user.id)
+                .or(Message.includes(:user)
+                    .where('user_id = ? AND recipient_id = ?', current_user.id, params[:id]))
+  end
+
+  def create; end
+
+  private
+
+  def messages_params
+    params.require(:message).permit(:id)
   end
 end
