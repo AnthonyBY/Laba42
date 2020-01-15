89d11e6e7a6631564a276b3a5c2458fa5cc1d855
Almix047 <Amix047@gmail.com>
2020-01-15 13:33:27 +0300

    Added message statuses (read, unread)

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,7 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :body_message, presence: true
+
+  AS_READ = { unread: 0, read: 1}.freeze
+  enum read_status: AS_READ, _prefix: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_063135) do
+ActiveRecord::Schema.define(version: 2020_01_15_071456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_063135) do
     t.integer "recipient_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "read_status", default: 0, null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
